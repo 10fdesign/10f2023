881a9eaf2116e0e23a963842345f19544f2577d0
Oscar Heller <oheller25@gmail.com>
2023-05-12 13:29:26 -0400

    Fixing dev setup. New layouts.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  // content: ["./*.html", "./_layouts/*.html", "./_includes/*.html"],
+  content: ["./_site/**/*.html"],
   theme: {
+    container: {
+      center: true,
+    },
     extend: {},
   },
   plugins: [],
